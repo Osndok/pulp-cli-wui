4f55dee5ac3810bfc901c7649a9a9b42a31c187e
Robert Hailey <git@osndok.com>
2025-12-04 16:21:22 -0600

    RoleCreate: name & permission are both required

--- src/main/java/com/github/osndok/pulp/cli/wui/pages/pulp/role/RoleCreate.java
+++ src/main/java/com/github/osndok/pulp/cli/wui/pages/pulp/role/RoleCreate.java
@@ -2,6 +2,7 @@ package com.github.osndok.pulp.cli.wui.pages.pulp.role;
 
 import com.github.osndok.pulp.cli.wui.base.BasicFormBasedExecution;
 import com.github.osndok.pulp.cli.wui.meta.TodoMultiValue;
+import org.apache.tapestry5.beaneditor.Validate;
 
 public
 class RoleCreate extends BasicFormBasedExecution
@@ -9,10 +10,12 @@ class RoleCreate extends BasicFormBasedExecution
     public static
     class Command
     {
+        @Validate("required")
         public String name;
         public String description;
         public Void no_permission;
         @TodoMultiValue
+        @Validate("required")
         public String permission;
     }
 
